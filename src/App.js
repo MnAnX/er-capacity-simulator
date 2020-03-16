d80dc8ffffe256dfef612cd8915bbece384e145d
Nan Xiao <nanxiao.xn@gmail.com>
2020-03-16 17:21:38 -0400

    add more info

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Config from './Config'
 import Control from './Control'
 import Summary from './Summary'
 import AllUnits from './AllUnits'
+import Padding from './components/Padding'
 
 const theme = createMuiTheme({
   palette: {
@@ -24,18 +25,22 @@ function App() {
     <ThemeProvider theme={theme}>
       <Container>
         <h1>COVID-19: ER Capacity Simulator</h1>
+        <p><a href='https://github.com/MnAnX/er-capacity-simulator'>github.com/MnAnX/er-capacity-simulator</a></p>
         <Container>
           <Paper>
             <Container>
+              <Padding height={4} />
               <h2>Configuration</h2>
               <br />
               <Config />
             </Container>
           </Paper>
         </Container>
+        <Padding height={20} />
         <Container>
           <Paper>
             <Container>
+              <Padding height={4} />
               <h2>Run Simulation</h2>
               <Control />
               <br />
